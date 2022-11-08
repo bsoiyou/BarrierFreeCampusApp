0f92b7e0f375b7978720feb3488e0a7d9bc62a07
ahwe <bsoiyou@naver.com>
2022-11-08 09:04:58 -0800

    Create Repo

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { auth } from './firebase';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+
 
 export default function App() {
+  //firebase test
+  //console.log(auth.currentUser);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <MapView
+        style={styles.map}
+        region={{
+            latitude: 51.5078788,
+            longitude: -0.0877321,
+            latitudeDelta: 0.001663,
+            longitudeDelta: 0.002001,
+        }}
+        provider={PROVIDER_GOOGLE}
+      >
+        <Marker
+        coordinate={{latitude: 51.5078788, longitude: -0.0877321}}
+        >
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -16,5 +36,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
