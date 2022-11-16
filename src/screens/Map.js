29e2d5af7ad1d800524563b57125db70f669c635
bsoiyou <bsoiyou@naver.com>
2022-11-16 04:48:34 -0800

    Map - Marker

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,71 +1,67 @@
-// import React, {useState, useEffect} from 'react';
-// import MapView,{Marker} from 'react-native-maps';
-// import { StyleSheet, Text, View, Dimensions } from 'react-native';
-// import { collection, query, onSnapshot} from "firebase/firestore";
-// import {DB} from '../firebase';
+import React, {useState, useEffect} from 'react';
+import MapView,{Marker} from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { addDoc, collection, query, onSnapshot, GeoPoint, getGeoPoint} from "firebase/firestore";
+import {DB} from '../firebase';
 
 
-// const Item= React.memo(
-  
-//   ({item: {title, description, userName, createdAt, uid, image, isEmer, id}, onPress}) => {
 
-//   return (
-//     <ItemContainer onPress={()=> onPress({title, description, userName, createdAt, uid, image, isEmer, id})}>
-//       <ItemTextContainer>
-//         <ItemTitle>{title}</ItemTitle>
-//       </ItemTextContainer>
-//       <ItemTime>{TimeStamp(createdAt)}</ItemTime>
-//       <ItemIcon />
-//     </ItemContainer>
-//   )
-// });
+export default function Map({navigation}) {
+  const [marks, setMarks] = useState([]);
 
-// export default function Map({navigation}) {
-//   const [marks, setMarks] = useState([]);
+  //markers collection에서 모든 문서 읽어와서 marks 배열에 저장
+  useEffect(()=> {
+    const q = query(collection(DB, "markers"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        list.push(new GeoPoint(doc.data().loc.latitude, doc.data().loc.longitude));
+      });
+      setMarks(list);
+    });
+    return ()=> unsubscribe();
+  }, []);
 
-//   // markers collection에서 모든 문서 읽어오기
-//   useEffect(()=> {
-//     const q = query(collection(DB, "markers"));
-//     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-//       const list = [];
-//       querySnapshot.forEach((doc) => {
-//         list.push(doc.data());
-//       });
-//       setMarks(list);
-//     });
-//     return ()=> unsubscribe();
-//   }, []);
 
-//   return (
-//     <View style={styles.container}>
-//       <MapView 
-//       style={styles.map} 
-//       initialRegion={{
-//           latitude: 37.78825,
-//           longitude: -122.4324,
-//           latitudeDelta: 0.0922,
-//           longitudeDelta: 0.0421,
-//       }}>
-//         {console.log(marks)}
-//         <Marker
-//           coordinate={{latitude: 37.78825, longitude: -122.4324}}
-//           title="this is a marker"
-//           description="this is a marker example"
-//         />
-//       </MapView>
-//     </View>
-//   );
-// }
+  return (
+    
+    <View style={styles.container}>
+      <MapView 
+      style={styles.map} 
+      initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+      }}>
+        {/* marks 배열에서 하나씩 꺼내서 marker 찍기 */}
+        {marks.map(
+          (item,index)=> {
+           return (
+            <Marker
+              key={index}
+              coordinate={{latitude: item.latitude, longitude: item.longitude}}
+              title={`${index}`}
+              description="this is a marker example"
+            />
+           );
+          
+          }
+        )}
+      </MapView>
+    </View>
+  );
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   map: {
-//     width: Dimensions.get('window').width,
-//     height: Dimensions.get('window').height,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
